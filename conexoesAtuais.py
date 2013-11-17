c9e0e8e5fcfa2455f31011acb4a23da6b7105c55
Sheila <sheila.dada@gmail.com>
2013-11-17 13:05:41 -0200

    Treating exception

--- conexoesAtuais.py
+++ conexoesAtuais.py
@@ -11,28 +11,23 @@ class ConexaoAtual:
   def adicionarConexao(self, ipCliente, portaCliente, ipRoteadorOrigem, ipRoteadorDestino, portaRoteadorDestino, portaClienteDestino):
 
     #tem a chave, esse cliente já possui
-    try:
-      if self.__clientesAtivos.get(ipCliente) is not None and self.__clientesAtivos[ipCliente].has_key(portaCliente):
-        raise ClienteOcupadoErro
-      elif not self.__clientesAtivos.has_key(ipCliente):
-        self.__clientesAtivos[ipCliente]={}
+    if self.__clientesAtivos.get(ipCliente) is not None and self.__clientesAtivos[ipCliente].has_key(portaCliente):
+      raise RuntimeError('Porta do cliente ocupada.')
+    elif not self.__clientesAtivos.has_key(ipCliente):
+      self.__clientesAtivos[ipCliente]={}
 
-      #ou não tem a chave cliente no primeiro dicionario ou não tem a porta cliente
-      #Ver se a porta ativa do roteador não é a mesma que eu quero colocar, se for, não adiciona nada
-      if self.__roteadoresAtivos.has_key(ipRoteadorDestino):
-        #lista de portas que estão sendo usadas
-        for porta in self.__roteadoresAtivos[ipRoteadorDestino]: 
-          if porta == portaRoteadorDestino:
-            raise DestinoOcupadoErro
-      else:
-        self.__roteadoresAtivos[ipRoteadorDestino] = []
-        #a porta não foi utilizada, adiciono nas portas ativas do roteador e adiciono a conexão nos clientes ativos
-      self.__roteadoresAtivos[ipRoteadorDestino].append(portaRoteadorDestino)
-      self.__clientesAtivos[ipCliente][portaCliente] = [ipRoteadorOrigem, ipRoteadorDestino, portaRoteadorDestino, portaClienteDestino]
-    except ClienteOcupadoErro:
-      print ("Porta do cliente ocupada.")
-    except DestinoOcupadoErro:
-      print ("Porta do roteador destino ocupada.")
+    #ou não tem a chave cliente no primeiro dicionario ou não tem a porta cliente
+    #Ver se a porta ativa do roteador não é a mesma que eu quero colocar, se for, não adiciona nada
+    if self.__roteadoresAtivos.has_key(ipRoteadorDestino):
+      #lista de portas que estão sendo usadas
+      for porta in self.__roteadoresAtivos[ipRoteadorDestino]: 
+        if porta == portaRoteadorDestino:
+          raise RuntimeError('Porta do roteador destino ocupada.')
+    else:
+      self.__roteadoresAtivos[ipRoteadorDestino] = []
+      #a porta não foi utilizada, adiciono nas portas ativas do roteador e adiciono a conexão nos clientes ativos
+    self.__roteadoresAtivos[ipRoteadorDestino].append(portaRoteadorDestino)
+    self.__clientesAtivos[ipCliente][portaCliente] = [ipRoteadorOrigem, ipRoteadorDestino, portaRoteadorDestino, portaClienteDestino]
 
   def printRoteadoresAtivos(self):
     print self.__roteadoresAtivos
--- testes.py
+++ testes.py
@@ -2,42 +2,50 @@
 # -*- coding: utf-8 -*- 
 
 import conexoesAtuais
+import sys
 
 conexoes = conexoesAtuais.ConexaoAtual()
 
 #adicionarConexao(self, ipCliente, portaCliente, ipRoteadorOrigem, ipRoteadorDestino, portaRoteadorDestino, portaClienteDestino)
-if (conexoes.adicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22222, 33333)):
-  print "Codigo correto"
+try:
+  conexoes.adicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22222, 33333)
+  print "Código correto"
   conexoes.printClientesAtivos()
   conexoes.printRoteadoresAtivos()
-
-else:
-  print "Erro no código"
+except Exception, err:
+  sys.stderr.write('ERRO: %s\n' % str(err)) 
+  print ("Não foi adicionada conexão. Código incorreto")
 
 #adiciona conexão para o mesmo ipcliente com porta do cliente diferente e porta do roteador destino diferente
-if (conexoes.adicionarConexao('10.0.0.1', 11112, '192.168.0.1', '192.168.0.2', 22221, 33333)):
-  print "Codigo correto"
+try:
+  conexoes.adicionarConexao('10.0.0.1', 11112, '192.168.0.1', '192.168.0.2', 22221, 33333)
+  print "Código correto"
   conexoes.printClientesAtivos()
   conexoes.printRoteadoresAtivos()
-
-else:
-  print "Erro no código"
+except Exception, err:
+  sys.stderr.write('ERRO: %s\n' % str(err)) 
+  print ("Não foi adicionada conexão. Código incorreto")
 
 #adiciona conexão para a mesma porta cliente, deve dar erro
-if (conexoes.adicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22223, 33333)):
-  print "Erro no código - aceitou a adição"
-
-else:
-  print "Código correto"
+try:
+  conexoes.adicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22223, 33333)
+  print "Código incorreto. Adicionou conexão."
+  conexoes.printClientesAtivos()
+  conexoes.printRoteadoresAtivos()
+except Exception, err:
+  sys.stderr.write('ERRO: %s\n' % str(err)) 
+  print ("Não foi adicionada conexão. Código correto.")
   conexoes.printClientesAtivos()
   conexoes.printRoteadoresAtivos()
 
 #adiciona conexão para a mesma porta roteador destino, deve dar erro
-if (conexoes.adicionarConexao('10.0.0.1', 11113, '192.168.0.1', '192.168.0.2', 22221, 33333)):
-  print "Erro no código - aceitou a adição"
-
-else:
-  print "Código correto"
+try:
+  conexoes.adicionarConexao('10.0.0.1', 11113, '192.168.0.1', '192.168.0.2', 22221, 33333)
+  print "Código incorreto. Adicionou conexão."
   conexoes.printClientesAtivos()
   conexoes.printRoteadoresAtivos()
-
+except Exception, err:
+  sys.stderr.write('ERRO: %s\n' % str(err)) 
+  print ("Não foi adicionada conexão. Código correto")
+  conexoes.printClientesAtivos()
+  conexoes.printRoteadoresAtivos()
