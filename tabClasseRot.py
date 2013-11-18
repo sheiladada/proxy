f4788e5f7feb90f5ac40de4d3eaa74b3ea257079
Sheila <sheila.dada@gmail.com>
2013-11-18 00:12:54 -0200

    class and its methods that will be implemented

--- tabClasseRot.py
+++ tabClasseRot.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+
+class TabelaClasseRoteador():
+
+  __tabelaClasseRot = {}
+
+  def adicionarRotClasse(self, classeIP, IPRoteador):
+  def deletarRotClasse(self, classeIP, IPRoteador):
+  def listarRotClasse(self, classeIP):
