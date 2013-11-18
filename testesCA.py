e4b59b2cde9233cc56435e43c89739d5248822c3
Sheila <sheila.dada@gmail.com>
2013-11-18 00:00:10 -0200

    import problem

--- testesCA.py
+++ testesCA.py
@@ -10,39 +10,39 @@ import testesfuncoesCA
 #adicionarConexao(self, ipCliente, portaCliente, ipRoteadorOrigem, ipRoteadorDestino, portaRoteadorDestino, portaClienteDestino)
 respostaEsperadaCliente = {'10.0.0.1': {11111: ['192.168.0.1', '192.168.0.2', 22222, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22222]}
-testesfuncoes.testeAdicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22222, 33333, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeAdicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22222, 33333, respostaEsperadaCliente, respostaEsperadaRot)
 
 #adiciona conexão para o mesmo ipcliente com porta do cliente diferente e porta do roteador destino diferente
 respostaEsperadaCliente = {'10.0.0.1': {11112: ['192.168.0.1', '192.168.0.2', 22221, 33333], 11111: ['192.168.0.1', '192.168.0.2', 22222, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22222, 22221]}
-testesfuncoes.testeAdicionarConexao('10.0.0.1', 11112, '192.168.0.1', '192.168.0.2', 22221, 33333, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeAdicionarConexao('10.0.0.1', 11112, '192.168.0.1', '192.168.0.2', 22221, 33333, respostaEsperadaCliente, respostaEsperadaRot)
 
 #adiciona conexão para a mesma porta cliente, deve dar exceção
 respostaEsperadaCliente = {'10.0.0.1': {11112: ['192.168.0.1', '192.168.0.2', 22221, 33333], 11111: ['192.168.0.1', '192.168.0.2', 22222, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22222, 22221]}
-testesfuncoes.testeAdicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22223, 33333, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeAdicionarConexao('10.0.0.1', 11111, '192.168.0.1', '192.168.0.2', 22223, 33333, respostaEsperadaCliente, respostaEsperadaRot)
 
 #adiciona conexão para a mesma porta roteador destino, deve dar exceção
 respostaEsperadaCliente = {'10.0.0.1': {11112: ['192.168.0.1', '192.168.0.2', 22221, 33333], 11111: ['192.168.0.1', '192.168.0.2', 22222, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22222, 22221]}
-testesfuncoes.testeAdicionarConexao('10.0.0.1', 11113, '192.168.0.1', '192.168.0.2', 22221, 33333, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeAdicionarConexao('10.0.0.1', 11113, '192.168.0.1', '192.168.0.2', 22221, 33333, respostaEsperadaCliente, respostaEsperadaRot)
 
 #deleta conexão normalmente
 respostaEsperadaCliente = {'10.0.0.1': {11112: ['192.168.0.1', '192.168.0.2', 22221, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22221]}
-testesfuncoes.testeDeletarConexao('10.0.0.1', 11111, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeDeletarConexao('10.0.0.1', 11111, respostaEsperadaCliente, respostaEsperadaRot)
 
 #deleta conexão com porta que não está no dicionário
 respostaEsperadaCliente = {'10.0.0.1': {11112: ['192.168.0.1', '192.168.0.2', 22221, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22221]}
-testesfuncoes.testeDeletarConexao('10.0.0.1', 11113, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeDeletarConexao('10.0.0.1', 11113, respostaEsperadaCliente, respostaEsperadaRot)
 
 #deleta conexão com ip cliente que não está no dicionário
 respostaEsperadaCliente = {'10.0.0.1': {11112: ['192.168.0.1', '192.168.0.2', 22221, 33333]}}
 respostaEsperadaRot = {'192.168.0.2': [22221]}
-testesfuncoes.testeDeletarConexao('10.0.0.2', 11113, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeDeletarConexao('10.0.0.2', 11113, respostaEsperadaCliente, respostaEsperadaRot)
 
 #deleta conexão com porta que não está no dicionário
 respostaEsperadaCliente = {}
 respostaEsperadaRot = {}
-testesfuncoes.testeDeletarConexao('10.0.0.1', 11112, respostaEsperadaCliente, respostaEsperadaRot)
+testesfuncoesCA.testeDeletarConexao('10.0.0.1', 11112, respostaEsperadaCliente, respostaEsperadaRot)
