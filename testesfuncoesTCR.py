b1388d94d30e307da7d8b8f1ef6a6b2c79145f19
Sheila <sheila.dada@gmail.com>
2013-11-20 14:58:57 -0200

    delete router from table function

--- testesfuncoesTCR.py
+++ testesfuncoesTCR.py
@@ -5,19 +5,19 @@ import tabClasseRot
 
 #Testes adicionarRotClasse, deletarRotClasse, listarRotClasse
 
-def testeAdicionarRotClasse(self, classeIP, IPRoteador, tabelaEsperada):
+def testeAdicionarRotClasse(classeIP, IPRoteador, tabelaEsperada):
   tabela = tabClasseRot.TabelaClasseRoteador()
   tabela.adicionarRotClasse(classeIP, IPRoteador)
   if tabela.getTabClasseRot() != tabelaEsperada:
     print "testeAdicionarRotClasse", classeIP, IPRoteador, "falhou, resposta esperada:", tabelaEsperada, "resposta dada:", tabela.getTabClasseRot()
 
-def testeDeletarRotClasse(self, classeIP, IPRoteador, tabelaEsperada):
+def testeDeletarRotClasse(classeIP, IPRoteador, tabelaEsperada):
   tabela = tabClasseRot.TabelaClasseRoteador()
   tabela.deletarRotClasse(classeIP, IPRoteador)
   if tabela.getTabClasseRot() != tabelaEsperada:
     print "testeDeletarRotClasse", classeIP, IPRoteador, "falhou, resposta esperada:", tabelaEsperada, "resposta dada:", tabela.getTabClasseRot()
 
-def testeListarRotClasse(self, classeIP, listaEsperada):
+def testeListarRotClasse(classeIP, listaEsperada):
   tabela = tabClasseRot.TabelaClasseRoteador()
   listaObtida = tabela.listarRotClasse(classeIP)
   if listaObtida != tabelaEsperada:
