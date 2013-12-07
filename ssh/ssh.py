ade00ea8065638a51284fd6aaa895164b823f36c
Sheila <sheila.dada@gmail.com>
2013-12-07 15:22:36 -0200

    funções conexão ssh

--- ssh/ssh.py
+++ ssh/ssh.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+
+import paramiko
+
+class Ssh():
+
+  __conexaoSsh = {}
+
+  def adicionar(self, ip, senha, usuario):
+    if self.__conexaoSsh.has_key(ip):
+      self.conexaoSsh[ip][2].close()
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip,port=222,username=usuario,password=senha)
+    conexaoSsh[ip]=[usuario, senha, ssh]
+
+  def fechar(self, ip):
+    self.conexaoSsh[ip][2].close()
+    del conexaoSsh[ip]
+
+  def fecharTodos(self):
+    for senha, usuario, conexao in self.__conexaoSsh.values():
+      conexao.close()
+    self.__conexaoSsh = {}
+
+  def getConexoes(self):
+    return self.__conexaoSsh
+
+  def getConexao(self, ip):
+    return return self.__conexaoSsh[ip][2]
+
--- ssh/testesSSH.py
+++ ssh/testesSSH.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+
+#fazer uma conexão
+#fazer várias conexões
+#fechar uma conexão
+#fechar várias conexões
+#devolver conexão a partir de um IP
+
+
