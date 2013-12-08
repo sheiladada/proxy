88a6400abd08dcaf7005d17fce994a23520a7ca9
Sheila <sheila.dada@gmail.com>
2013-12-08 01:38:29 -0200

    testes de enviar regras

--- cmdRoteador/cmdRoteador.py
+++ cmdRoteador/cmdRoteador.py
@@ -21,31 +21,34 @@ class CmdRot():
     else:
       return True
 
+  def insereRegras(self, comandos, ssh, rot):
+
+    # comandos que irão escrever no arquivo do iptables
+    comandosArq = ''
+    arqIptables = '/etc/rc.d/rc.local'
+
+    for comando in comandos:
+      ssh.exec_command(comando)
+      comandoArq = comando.replace('iptables', '$IPT')
+      comandosArq = comandosArq + '\n' + comandoArq
+
+    ftp = ssh.open_sftp()
+    ftp.get(arqIptables, rot)
+    #abrir arquivo, editar e fechar
+    f = open(rot,'a')
+    f.write(comandosArq)
+    f.close()
+    ftp.put(rot, arqIptables)
+    ftp.close()
+
   def escreveRegras(self, ipO,rotO,portaLO,rotD, portaLD, ipD,portaD, sshO, sshD):
 
     arqIptables = '/etc/rc.d/rc.local'
 
-    comandoRO = ["iptables -t -nat -A PREROUTING -s " + ipO + " -d " + rotO + " -p tcp  --dport " + portaLO + " -i $GREEN_DEV -j DNAT --to " + rotD + ":" + portaLD, "iptables -t -nat -A POSTROUTING -s " + ipO + " -d " + rotD + " -p tcp -o $RED_DEV -j SNAT --to-source " + rotO]
-    comandoRD = ["iptables -t -nat -A PREROUTING -s " + rotO + " -d " + rotD + " -p tcp  --dport " + portaLD + " -i $RED_DEV -j DNAT --to " + ipD + ":" + portaD, "iptables -t -nat -A POSTROUTING -s " + rotO + " -d " + ipD + " -p tcp -o $GREEN_DEV -j SNAT --to-source " + rotD]
+    comandoRO = ["iptables -t -nat -A PREROUTING -s " + ipO + " -d " + rotO + " -p tcp  --dport " + str(portaLO) + " -i $GREEN_DEV -j DNAT --to " + rotD + ":" + str(portaLD), "iptables -t -nat -A POSTROUTING -s " + ipO + " -d " + rotD + " -p tcp -o $RED_DEV -j SNAT --to-source " + rotO]
+    comandoRD = ["iptables -t -nat -A PREROUTING -s " + rotO + " -d " + rotD + " -p tcp  --dport " + str(portaLD) + " -i $RED_DEV -j DNAT --to " + ipD + ":" + str(portaD), "iptables -t -nat -A POSTROUTING -s " + rotO + " -d " + ipD + " -p tcp -o $GREEN_DEV -j SNAT --to-source " + rotD]
 
-    insereRegras(comandoRO, sshO)
-    insereRegras(comandoRD, sshD)
+    self.insereRegras(comandoRO, sshO, rotO)
+    self.insereRegras(comandoRD, sshD, rotD)
 
-  def insereRegras(self, comandos, ssh):
 
-    # comandos que irão escrever no arquivo do iptables
-    comandosArq = ''
-
-    for comando in comandos:
-      sshO.exec_command(comando)
-      comandoArq = comando.replace('iptables', '$IPT')
-      comandosArq = comandosArqO + '\n' + comandoArq
-
-    ftp = ssh.open_sftp()
-    ftp.get(arqIptables, ipO)
-    #abrir arquivo, editar e fechar
-    f = open(ipO,'a')
-    f.write(comandosArq)
-    f.close()
-    ftp.put(ipO, arqIptables)
-    ftp.close()
